d9b0b51e5c7b7c49671cda7240dcdb8e1dfdc240
ahmed666916 <ahmed666916@gmail.com>
2024-02-01 20:14:41 +0500

    accessing, changing and deleting object properties

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 let user = {     // an object
     name: "John",  // by key "name" store value "John"
-    age: 30        // by key "age" store value 30
+    surname: "Smith",
+    age: 30 ,       // by key "age" store value 30
+    "likes birds": true  // multiword property name must be quoted
   };
 
+
+user.name = "Ahmed";
+
 alert(user.name);
+
+delete user.surname;
+
+console.log(user.name);
+console.log(user.surname);
+console.log(user.age);
+console.log(user["likes birds"]);
